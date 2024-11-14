e859a81f115dc64f71c447b238cdc0cbd332b314
Berresu <90646787+Berresu@users.noreply.github.com>
2024-11-14 21:19:40 +0300

    Create vizeFinalNotuOrtalamasi.cs

---
+++ vizeFinalNotuOrtalamasi.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programlamayaGirisVize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vize Notunuzu Giriniz: ");
+            int vizeNotu = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Final Notunuzu Giriniz: ");
+            int finalNotu = Convert.ToInt32(Console.ReadLine());
+
+            int ortalama = ((vizeNotu * 40) / 100) + ((finalNotu * 60) / 100);
+            Console.WriteLine("Ortalamanız: " + ortalama);
+
+            if (finalNotu < 45)
+            {
+                Console.WriteLine("Final Notunuz 45'in Altında Olduğu İçin Kaldınız.");
+            }
+            else if (ortalama >= 50)
+            {
+                Console.WriteLine("Not Ortalamanız 50 Veya 50'den Yüksek Olduğu İçin Geçtiniz.");
+            }
+            else
+            {
+                Console.WriteLine("Not Ortalamanız 50'den Düşük Olduğu İçin Kaldınız.");
+            }
+        }
+    }
+}
